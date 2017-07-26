aed1e91019be7fa1745de1cc221b73a2a529eff1
vagrant <vagrant@localhost.localdomain>
2017-07-26 05:23:54 +0000

    Initial cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,12 @@
+package 'tree' do
+ action :install
+end
+package 'ntp' do
+ action :install
+end
+package 'git'
+file '/etc/motd' do
+ content 'This computer is a property of HPE'
+ owner 'vagrant'
+ group 'vagrant'
+end
